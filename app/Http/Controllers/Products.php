f7dc5209bf454919a9ea9df4b46bfad6c3617da1
Bucurenciu Bogdan Cristian <bucurenciu.cristian@gmail.com>
2022-08-03 14:20:21 +0300

    The controller is now ready to work.

--- app/Http/Controllers/Products.php
+++ app/Http/Controllers/Products.php
@@ -20,12 +20,13 @@ class Products extends Controller
     public function index()
     {
         return view('products.index',
-            ["products" => Product::latest()->paginate(3)]);
+            ["products" => Product::latest()->simplePaginate(5)]
+        );
     }
 
-    public function show()
+    public function show(Product $product)
     {
-        return response("Salut", 404);
+        return view('products.show',['product'=> $product]);
     }
 
     public function create()
@@ -33,18 +34,39 @@ class Products extends Controller
         return view('products.create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
-
+        $formFields = $request->validate([
+//            'title' => ['required', Rule::unique('products','title')],
+            'title' => 'required',
+            'description' => 'required',
+            'price' => ['required','numeric'],
+            'quantity' => ['required','numeric'],
+        ]);
+        Product::create($formFields);
+        return redirect('/')->with('message', 'Product added succesfully');
     }
 
-    public function update()
+    public function update(Request $request, Product $product)
     {
-
+        $formFields = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'quantity' => 'required',
+        ]);
+        $product->update($formFields);
+        return redirect('/')->with('message', 'Product Updated Successfully');
     }
 
-    public function destroy()
+    public function destroy(Product $product)
     {
-
+        $product->delete();
+        return redirect('/')->with('message', 'Product deleted successfully');
+    }
+    //Show Edit Form
+    public function edit(Product $product)
+    {
+        return view('products.edit', ['product' => $product]);
     }
 }
